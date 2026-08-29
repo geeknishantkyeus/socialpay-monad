597afa67eba589c30f4e4282604f8567f284296c
geeknishantkyeus <nishnat4567128@gmail.com>
2026-08-29 17:20:53 +0530

    Fix final Vercel build

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,7 +2,7 @@
 import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { wagmiConfig } from '../lib/config'
+import { wagmiConfig } from '../../lib/config'
 import '@rainbow-me/rainbowkit/styles.css'
 
 const queryClient = new QueryClient()
